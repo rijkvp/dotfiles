796bdc4e40a7d2e26b72f79b68776a47db9ebf9d
rijkvp <github@rijkvp.nl>
2021-11-29 17:14:15 +0100

    Add Kitty & Librewolf

---
+++ .config/polybar/scripts/toggle_details.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+polybar-msg action "#networkspeed.toggle"
+polybar-msg action "#memory.toggle"
+polybar-msg action "#cpu.toggle"
+polybar-msg action "#gpu.toggle"
+polybar-msg action "#ram.toggle"
---
+++ .config/polybar/scripts/weather.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# temp=$(curl --silent "wttr.in/${WEATHER_CITY}?format=%t" | cut -c2-6)
+ftemp=$(curl --silent "wttr.in/${WEATHER_CITY}?format=%f" | cut -c2-6)
+precipitation=$(curl --silent "wttr.in/${WEATHER_CITY}?format=%p" | cut -c1-6)
+echo $ftemp $precipitation
