eee5f3a34bded63534b45004b1d561475f9a8f0b
rijkvp <github@rijkvp.nl>
2022-01-01 16:58:00 +0100

    Add Dracula GTK theme

--- .config/polybar/scripts/weather.sh
+++ .config/polybar/scripts/weather.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# temp=$(curl --silent "wttr.in/${WEATHER_CITY}?format=%t" | cut -c2-6)
-ftemp=$(curl --silent "wttr.in/${WEATHER_CITY}?format=%f" | cut -c2-6)
+temp=$(curl --silent "wttr.in/${WEATHER_CITY}?format=%t" | cut -c1-6)
+#ftemp=$(curl --silent "wttr.in/${WEATHER_CITY}?format=%f" | cut -c2-6)
 precipitation=$(curl --silent "wttr.in/${WEATHER_CITY}?format=%p" | cut -c1-6)
-echo $ftemp $precipitation
+echo $temp $precipitation
