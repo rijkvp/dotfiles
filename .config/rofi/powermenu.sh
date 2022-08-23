4ec5819b8c1dec6a8faefde4389b5bcc6147a20a
rijkvp <rijk@rijkvp.nl>
2022-08-23 14:51:59 +0200

    Add rofi theme

---
+++ .config/rofi/powermenu.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# Very basic powermenu script for Rofi
+# Run with: rofi -show pm -modes "pm:powermenu.sh"
+
+if [ x"$@" = x"Lock" ]
+then
+    slock
+    exit 0
+fi
+if [ x"$@" = x"Suspend" ]
+then
+    loginctl suspend
+    exit 0
+fi
+if [ x"$@" = x"Shutdown" ]
+then
+    loginctl poweroff
+    exit 0
+fi
+if [ x"$@" = x"Reboot" ]
+then
+    reboot
+    exit 0
+fi
+
+echo "\0prompt\037Power menu\n"
+
+echo "Lock"
+echo "Suspend"
+echo "Shutdown"
+echo "Reboot"
+
